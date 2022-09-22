b01a53893915c2ae58f7837552fe2f644879e52b
vadim <vadimirmmu@gmail.com>
2022-09-22 18:14:54 +0300

    дз

---
+++ Desktop/git/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите целые числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2==0)
+{
+    Console.WriteLine("четные");
+}
+else
+{
+     Console.WriteLine("не четные");
+}
